bcdd412af8c5ef6da36ffa28244026e9573575a9
John Bellone <john.bellone.jr@gmail.com>
2014-07-22 12:53:19 -0400

    Fix problems with setting default user/group.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,3 @@ default['ruby-install']['git_ref'] = 'v0.4.3'
 default['ruby-install']['install_path'] = nil
 default['ruby-install']['rubies'] = []
 default['ruby-install']['gems'] = []
-default['ruby-install']['user'] = 'root'
-default['ruby-install']['group'] = 'root'
